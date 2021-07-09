6137135ccba9217b40bcb34247b799cbd8725b64
Julian-Calle <jcallecristancho@gmail.com>
2021-07-09 06:20:10 +0200

    test

--- server.js
+++ server.js
@@ -88,7 +88,7 @@ app.use((error, req, res, next) => {
 });
 
 // if (process.env.NODE_ENV === "production") {
-app.use(express.static(path.join(__dirname, "front", "build")));
+app.use(express.static(path.join(__dirname, "front/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "front", "build", "index.html"));
 });
