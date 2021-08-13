ab11c3901479ae85d765b2314b53aa1d497f0595
Julian-Calle <jcallecristancho@gmail.com>
2021-08-13 13:22:01 +0200

    swagger update

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const pathApi = process.env.PUBLIC_HOST;
 const swaggerOptions = {
   swaggerDefinition: {
     info: {
-      description: `API was mented to be used for tracking product from [Thomann website](https://www.thomann.de/es/index.html). 
+      description: `API was mented to be used for tracking products from [Thomann website](https://www.thomann.de/es/index.html). 
       In the following interactive End point you can test this API.
       There is a basic example of front application [HERE](https://juuglepricetracker.vercel.app/).
       `,
